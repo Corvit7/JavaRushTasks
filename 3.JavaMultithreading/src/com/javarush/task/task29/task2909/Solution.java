accaa6f85a7b13bed38cef2a556ec5468d7e36a0
Corvit <vcfrolov@gmail.com>
2020-07-17 05:06:28 +0300

    refactoring(8)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task29.task2909;
 
 import com.javarush.task.task29.task2909.human.Student;
+import com.javarush.task.task29.task2909.human.StudentsDataBase;
 import com.javarush.task.task29.task2909.human.University;
 
 import java.util.LinkedList;
@@ -13,13 +14,22 @@ public class Solution {
         students.add(new Student("Mark", 22, 3.8));
         students.add(new Student("Jack", 20, 4.0));
 
+        StudentsDataBase.addInfoAboutStudent(students.get(0));
+        StudentsDataBase.addInfoAboutStudent(students.get(1));
+        StudentsDataBase.addInfoAboutStudent(students.get(2));
+        StudentsDataBase.addInfoAboutStudent(new Student("Dima", 22, 4.4));
+        StudentsDataBase.removeStudent(5);
+        StudentsDataBase.findDimaOrSasha();
 
-        University oxford = new University("Oxford", 750);
-        oxford.setStudents(students);
+
+
+
+//        University oxford = new University("Oxford", 750);
+//        oxford.setStudents(students);
 //        System.out.println(oxford.getStudentWithAverageGrade(2).getName());
 //        System.out.println(oxford.getStudentWithMaxAverageGrade().getName());
-        System.out.println(oxford.getStudentWithMinAverageGrade().getName());
-        oxford.expel(oxford.getStudentWithMinAverageGrade());
-        System.out.println(oxford.getStudentWithMinAverageGrade().getName());
+//        System.out.println(oxford.getStudentWithMinAverageGrade().getName());
+//        oxford.expel(oxford.getStudentWithMinAverageGrade());
+//        System.out.println(oxford.getStudentWithMinAverageGrade().getName());
     }
 }
