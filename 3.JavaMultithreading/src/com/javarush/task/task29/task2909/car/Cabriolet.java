9c04187623ff208c9da0a61491994d6ce7d3b25a
Corvit <vcfrolov@gmail.com>
2020-07-17 06:27:51 +0300

    refactoring(12)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Cabriolet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Cabriolet.java
@@ -4,4 +4,9 @@ public class Cabriolet extends Car {
     public Cabriolet(int numberOfPassengers) {
         super(2, numberOfPassengers);
     }
+
+    @Override
+    public int getMaxSpeed() {
+        return Car.MAX_CABRIOLET_SPEED;
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
@@ -4,10 +4,14 @@ import com.sun.scenario.effect.impl.sw.java.JSWBlend_HARD_LIGHTPeer;
 
 import java.util.Date;
 
-public class Car {
+public abstract class Car {
     static public final int TRUCK = 0;
     static public final int SEDAN = 1;
     static public final int CABRIOLET = 2;
+
+    static public final int MAX_TRUCK_SPEED = 80;
+    static public final int MAX_SEDAN_SPEED = 120;
+    static public final int MAX_CABRIOLET_SPEED = 90;
 
     double fuel;
 
@@ -67,9 +71,12 @@ public class Car {
     }
 
     public int getNumberOfPassengersCanBeTransferred() {
-        if (!isDriverAvailable())
-            return 0;
-        if (fuel <= 0)
+//        if (!isDriverAvailable())
+//            return 0;
+//        if (fuel <= 0)
+//            return 0;
+
+        if(!canPassengersBeTransferred())
             return 0;
 
         return numberOfPassengers;
@@ -84,12 +91,9 @@ public class Car {
     }
 
     public void startMoving() {
-        if (numberOfPassengers > 0) {
+        fastenDriverBelt();
+        if (numberOfPassengers > 0)
             fastenPassengersBelts();
-            fastenDriverBelt();
-        } else {
-            fastenDriverBelt();
-        }
     }
 
     public void fastenPassengersBelts() {
@@ -98,13 +102,14 @@ public class Car {
     public void fastenDriverBelt() {
     }
 
-    public int getMaxSpeed() {
-        if (type == TRUCK)
-            return 80;
-        if (type == SEDAN)
-            return 120;
-        return 90;
-    }
+    public abstract int getMaxSpeed();
+//    public abstract int getMaxSpeed() {
+//        if (type == TRUCK)
+//            return 80;
+//        if (type == SEDAN)
+//            return 120;
+//        return 90;
+//    }
 
     public boolean isSummer(Date date, Date summerStart, Date summerEnd) {
         if (date.after(summerStart) && date.before(summerEnd))
@@ -119,5 +124,13 @@ public class Car {
 
     public double getSummerConsumption(int length){
         return length * summerFuelConsumption;
+    }
+
+    private boolean canPassengersBeTransferred()
+    {
+        if(driverAvailable && fuel > 0)
+            return true;
+        else
+            return false;
     }
 }
