c434c4c127cbcee99d24963fc022891133aa6a59
Corvit <vcfrolov@gmail.com>
2020-07-18 08:32:55 +0300

    task2603

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
@@ -0,0 +1,84 @@
+package com.javarush.task.task26.task2603;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/*
+Убежденному убеждать других не трудно
+*/
+//Убежденному убеждать других не трудно
+//        В таблице есть колонки, по которым можно сортировать.
+//        Пользователь имеет возможность настроить под себя список колонок, которые будут сортироваться.
+//        Напиши public static компаратор CustomizedComparator, который будет:
+//        1. В конструкторе принимать массив компараторов.
+//        2. Сортировать данные в порядке, соответствующем последовательности компараторов.
+//        Все переданные компараторы сортируют дженерик тип Т.
+//        В конструктор передается как минимум один компаратор.
+//
+//
+//        Требования:
+//        1. Класс Solution должен содержать public static компаратор CustomizedComparator.
+//        2. Класс CustomizedComparator должен содержать приватное поле comparators типа Comparator<T>[].
+//        3. Класс CustomizedComparator должен содержать конструктор с параметром vararg компараторов.
+//        4. Метод compare() класса CustomizedComparator должен сравнивать объекты в порядке, соответствующем последовательности компараторов comparators.
+public class Solution {
+
+    public static class CustomizedComparator<T> implements Comparator<T>{
+
+        private Comparator<T>[] comparators;
+
+        public CustomizedComparator(Comparator... comparators) {
+            this.comparators = comparators;
+        }
+
+        @Override
+        public int compare(T o1, T o2)
+        {
+            int result = 0;
+            for (Comparator comparator : comparators)
+            {
+                result = comparator.compare(o1, o2);
+                if (result != 0) break; //если аргументы равны, переходим к следующему компаратору (критерию сравнения), если разные возвращаем результат сравнения
+            }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Woman> women = new ArrayList<Woman>();
+        women.add(new Woman(18, 0, 45, 117, "Ann"));
+        women.add(new Woman(21, 1, 57, 118, "Iren"));
+        women.add(new Woman(5, 0, 60, 110, "Angelina"));
+
+        Comparator<Woman> compareByHeight = new Comparator<Woman>() {
+
+            public int compare(Woman o1, Woman o2) {
+                return o1.height - o2.height;
+            }
+        };
+
+        Comparator<Woman> compareByWeight = new Comparator<Woman>() {
+
+            public int compare(Woman o1, Woman o2) {
+                return o1.weight - o2.weight;
+            }
+        };
+
+//        ArrayList<Comparator> comparators = new ArrayList<>();
+//        comparators.add(compareByHeight);
+//        comparators.add(compareByWeight);
+        Comparator<Woman>[] comparators = new Comparator[2];
+        comparators[0] = compareByHeight;
+        comparators[1] = compareByWeight;
+
+        CustomizedComparator<Woman> comparator = new CustomizedComparator<>(comparators);
+
+        Collections.sort(women,comparator);
+
+//        for (int i = 0; i < women.size(); i++) {
+//            System.out.println(women.get(i).name);
+//        }
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Woman.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task26.task2603;
+
+public class Woman {
+
+    public int age;
+    public int childrenCount;
+    public int weight;
+    public int height;
+    public String name;
+
+    public Woman(int age, int childrenCount, int weight, int height, String name) {
+
+        this.age = age;
+        this.childrenCount = childrenCount;
+        this.weight = weight;
+        this.height = height;
+        this.name = name;
+    }
+}
