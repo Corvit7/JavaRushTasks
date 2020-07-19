24173b76b747ab4c7239aecac8f5da2f13cc9710
Corvit <vcfrolov@gmail.com>
2020-07-19 22:41:59 +0300

    chat(2)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,8 +1,16 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+
 public class Server {
 
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+//        String str = ConsoleHelper.readString();
+//        System.out.println(str);
+
+        int i = ConsoleHelper.readInt();
+        System.out.println(i);
     }
 }
