3ddd00e939974ac6620c58439bd12a0c0d5d7359
Corvit <vcfrolov@gmail.com>
2020-07-23 10:04:59 +0300

    chat(21)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/ClientGuiController.java
@@ -0,0 +1,142 @@
+//package com.javarush.task.task30.task3008.client;
+//
+//public class ClientGuiController extends Client {
+//    ClientGuiModel clientGuiModel;
+//    ClientGuiView clientGuiView;
+//
+//    public ClientGuiController() {
+//        this.clientGuiModel = new ClientGuiModel();
+//        this.clientGuiView = new ClientGuiView(this);
+//    }
+//
+//    public class GuiSocketThread extends Client.SocketThread{
+//        @Override
+//        protected void processIncomingMessage(String message) {
+//            clientGuiModel.setNewMessage(message);
+//            clientGuiView.refreshMessages();
+//        }
+//
+//        @Override
+//        protected void informAboutAddingNewUser(String userName) {
+//            clientGuiModel.addUser(userName);
+//            clientGuiView.refreshUsers();
+//        }
+//
+//        @Override
+//        protected void informAboutDeletingNewUser(String userName) {
+//            clientGuiModel.deleteUser(userName);
+//            clientGuiView.refreshUsers();
+//        }
+//
+//        @Override
+//        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+//            clientGuiView.notifyConnectionStatusChanged(clientConnected);
+//        }
+//    }
+//
+//    @Override
+//    protected SocketThread getSocketThread() {
+//        return new GuiSocketThread();
+//    }
+//
+//    @Override
+//    public void run() {
+//        getSocketThread().run();
+//    }
+//
+////    Методы getServerAddress(), getServerPort(), getUserName() в классе ClientGuiController должны вызывать соответствующие методы у объекта view.
+//    @Override
+//    protected String getServerAddress() {
+//        return this.clientGuiView.getServerAddress();
+//    }
+//
+//    @Override
+//    protected int getServerPort() {
+//        return this.clientGuiView.getServerPort();
+//    }
+//
+//    @Override
+//    protected String getUserName() {
+//        return this.clientGuiView.getUserName();
+//    }
+//
+//    public ClientGuiModel getModel()
+//    {
+//        return this.clientGuiModel;
+//    }
+//
+//    public static void main(String[] args) {
+//        ClientGuiController clientGuiController = new ClientGuiController();
+//        clientGuiController.run();
+//    }
+//}
+
+
+
+package com.javarush.task.task30.task3008.client;
+
+public class ClientGuiController extends Client{
+    private ClientGuiModel model = new ClientGuiModel();
+    private ClientGuiView view = new ClientGuiView(this);
+
+    public class GuiSocketThread extends SocketThread{
+        @Override
+        protected void processIncomingMessage(String message){
+            model.setNewMessage(message);
+            view.refreshMessages();
+        }
+
+        @Override
+        protected void informAboutAddingNewUser(String userName){
+            model.addUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        protected void informAboutDeletingNewUser(String userName){
+            model.deleteUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        protected void notifyConnectionStatusChanged(boolean clientConnected){
+            super.notifyConnectionStatusChanged(clientConnected);
+            view.notifyConnectionStatusChanged(clientConnected);
+        }
+    }
+
+    @Override
+    protected String getServerAddress(){
+        return view.getServerAddress();
+    }
+
+    @Override
+    protected int getServerPort(){
+        return view.getServerPort();
+    }
+
+    @Override
+    protected String getUserName(){
+        return view.getUserName();
+    }
+
+    @Override
+    protected SocketThread getSocketThread(){
+        return new GuiSocketThread();
+    }
+
+    @Override
+    public void run(){
+        SocketThread socketThread = getSocketThread();
+        socketThread.run();
+    }
+
+    public ClientGuiModel getModel () {
+        return model;
+    }
+
+    public static void main(String[] args){
+        ClientGuiController guiController = new ClientGuiController();
+        guiController.run();
+    }
+}
