d48f241784be7c7d7b7ad6c9408983dc5b7f5c86
Corvit <vcfrolov@gmail.com>
2020-07-29 16:44:11 +0300

    task31.task3110 Archiver(13)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
@@ -1,26 +1,18 @@
 package com.javarush.task.task31.task3110;
 
-
-//Свойства - это набор, состоящий из: имя файла, размер файла до и после сжатия, метод сжатия.
-//1. Создай класс FileProperties
-//2. Добавь в него приватные переменные класса:
-//2.1. Имя String name
-//2.2. Размер в байтах long size
-//2.3. Размер после сжатия в байтах long compressedSize
-//2.4. Метод сжатия int compressionMethod
-//3. Добавь гетеры для них
-//4. Добавь конструктор FileProperties(String name, long size, long compressedSize, int compressionMethod)
-//5. Добавь метод long getCompressionRatio(), который будет считать степень сжатия по формуле: 100 - ((compressedSize * 100) / size)
-//6. Переопредели метод String toString(), чтобы он возвращал строку по шаблону:
-//"name size Kb (compressedSize Kb) сжатие: compressionRatio%", если размер size больше нуля, иначе он должен вернуть только имя файла.
-//Нулевой размер может быть, например, у директории. Не забудь перевести байты в килобайты, а их не столько же, сколько граммов в килограмме, и даже не столько, сколько блинов у меня на столе... Хм, похоже мне пора перекусить...
-
 public class FileProperties {
-
     private String name;
     private long size;
     private long compressedSize;
     private int compressionMethod;
+
+    public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
+        this.name = name;
+        this.size = size;
+        this.compressedSize = compressedSize;
+        this.compressionMethod = compressionMethod;
+    }
+
 
     public String getName() {
         return name;
@@ -38,38 +30,26 @@ public class FileProperties {
         return compressionMethod;
     }
 
-    public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
-        this.name = name;
-        this.size = size;
-        this.compressedSize = compressedSize;
-        this.compressionMethod = compressionMethod;
-    }
-
-//    Добавь метод long getCompressionRatio(), который будет считать степень сжатия по формуле: 100 - ((compressedSize * 100) / size)
-    public long getCompressionRatio()
-    {
+    public long getCompressionRatio() {
+        // Вычисляем степень сжатия
         return 100 - ((compressedSize * 100) / size);
     }
 
-    //6. Переопредели метод String toString(), чтобы он возвращал строку по шаблону:
-//"name size Kb (compressedSize Kb) сжатие: compressionRatio%", если размер size больше нуля, иначе он должен вернуть только имя файла.
-//Нулевой размер может быть, например, у директории. Не забудь перевести байты в килобайты, а их не столько же, сколько граммов в килограмме, и даже не столько, сколько блинов у меня на столе... Хм, похоже мне пора перекусить...
-
-
     @Override
     public String toString() {
-//        "name size Kb (compressedSize Kb) сжатие: compressionRatio%"
-        String template = "%s %d Kb (%d Kb) сжатие: %d%%";
-        if (size > 0)
-            return String.format(template, this.name, this.size / 1024, this.compressedSize / 1024, this.getCompressionRatio());
-        else
-            return name;
-    }
+        // Строим красивую строку из свойств
+        StringBuilder builder = new StringBuilder();
+        builder.append(name);
+        if (size > 0) {
+            builder.append("\t");
+            builder.append(size / 1024);
+            builder.append(" Kb (");
+            builder.append(compressedSize / 1024);
+            builder.append(" Kb) сжатие: ");
+            builder.append(getCompressionRatio());
+            builder.append("%");
+        }
 
-//    public static void main(String[] args) {
-//        FileProperties fileProperties = new FileProperties("test", 10000000, 5000000, 4);
-//        System.out.println(fileProperties);
-//        System.out.println(new FileProperties("test2", 0, 0, 8));
-//
-//    }
+        return builder.toString();
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -1,15 +1,19 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.FileProperties;
 import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.util.Iterator;
 
 public class ZipContentCommand extends ZipCommand{
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Содержимое архива");
+        ZipFileManager zipFileManager = getZipFileManager();
 
-    }
-
-    @Override
-    public ZipFileManager getZipFileManager() throws Exception {
-        return null;
+        Iterator<FileProperties> iterator = zipFileManager.getFilesList().iterator();
+        while(iterator.hasNext())
+            System.out.println(iterator.next().getName());
     }
 }
