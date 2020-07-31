3f1f18b8b7e80dc6a4c7e0640b2bb137734d6e01
Corvit <vcfrolov@gmail.com>
2020-07-31 11:19:26 +0300

    task31.taks3209 HTML Editor(3)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,11 +1,28 @@
 package com.javarush.task.task32.task3209;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
+//3.1. Добавь и проинициализируй поля в классе представления:
+//        3.1.1. JTabbedPane tabbedPane - это будет панель с двумя вкладками.
+//        3.1.2. JTextPane htmlTextPane - это будет компонент для визуального редактирования html.
+//        Он будет размещен на первой вкладке.
+//        3.1.3. JEditorPane plainTextPane - это будет компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое).
+
 public class View extends JFrame implements ActionListener {
     private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+
+//    public View() throws HeadlessException {
+//        tabbedPane = new JTabbedPane();
+//        htmlTextPane = new JTextPane();
+//        plainTextPane = new JEditorPane();
+//    }
 
     public Controller getController() {
         return controller;
