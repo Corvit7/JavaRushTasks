5fe31dd7e3733d0e760054e9aed186caf9178113
Corvit <vcfrolov@gmail.com>
2020-07-31 11:09:23 +0300

    task32.task3209 Archiver(2)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -12,7 +12,32 @@ public class Controller {
         this.view = view;
     }
 
-    public static void main(String[] args) {
-        
+    public void exit()
+    {
+        System.exit(0);
     }
+
+
+//    2.2. Теперь напишем в классе Controller метод main().
+//    Он должен:
+//            2.2.1. Создавать объект представления.
+//2.2.2. Создавать контроллер, используя представление.
+//2.2.3. Устанавливать у представления контроллер.
+//            2.2.4. Инициализировать представление.
+//            2.2.5. Инициализировать контроллер. Контроллер должен инициализироваться после представления.
+//2.3. Добавь в контроллер метод exit(), он должен вызывать статический метод exit у класса System.
+//            2.3.1. Метод exit в классе Controller не должен быть статическим.
+//2.4. Добавь в представление метод exit(), он должен вызывать exit() у контроллера.
+//
+
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+        view.setController(controller);
+        view.init();
+        controller.init();
+
+    }
+
+    public void init(){}
 }
