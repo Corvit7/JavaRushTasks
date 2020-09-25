eb21656f3184932c8991830712c8a1971cb28a80
Corvit <vcfrolov@gmail.com>
2020-09-25 11:06:33 +0300

    task.taks2309 HTML Editor(9)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -17,6 +17,15 @@ public class View extends JFrame implements ActionListener {
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
 
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 
     public Controller getController() {
         return controller;
@@ -43,7 +52,38 @@ public class View extends JFrame implements ActionListener {
         controller.exit();
     }
 
-    public void initMenuBar(){}
+
+//    HTML Editor (9)
+//    9.1. Реализуй метод initMenuBar(). Он должен:
+//    9.1.1. Создавать новый объект типа JMenuBar. Это и будет наша панель меню.
+//    9.1.2. С помощью MenuHelper инициализировать меню в следующем порядке: Файл, Редактировать, Стиль, Выравнивание, Цвет, Шрифт и Помощь.
+//    9.1.3. Добавлять в верхнюю часть панели контента текущего фрейма нашу панель меню, аналогично тому, как это мы делали с панелью вкладок.
+//    9.2. Добавь конструктор класса View. Он должен устанавливать внешний вид и поведение (look and feel) нашего приложения такими же, как это определено в системе.
+//    Конструктор не должен кидать исключений, только логировать их с помощью ExceptionHandler.
+//
+//    Подсказа: для реализации задания используй класс UIManager.
+//
+//    Запусти приложение, теперь ты должен видеть панель с меню вверху окна. Некоторые из пунктов меню (например: Вырезать, Копировать, Вставить, Стиль (частично), Выравнивание, Цвет, Шрифт) должны уже работать. Убедись, что все работает и только затем продолжи разработку.
+//
+//
+//    Требования:
+//            1. В методе initMenuBar() должно создаваться новое меню (объект типа JMenuBar).
+//            2. В методе initMenuBar() c помощью MenuHelper должно быть проинициализировано меню в следующем порядке: Файл, Редактировать, Стиль, Выравнивание, Цвет, Шрифт и Помощь.
+//            3. В методе initMenuBar() должно добавляться новосозданное меню в верхнюю часть панели контента текущего фрейма, используя метод getContentPane().
+//            4. В конструкторе класса View
+
+    public void initMenuBar(){
+        JMenuBar jMenuBar = new JMenuBar();
+        MenuHelper.initFileMenu(this, jMenuBar);
+        MenuHelper.initEditMenu(this, jMenuBar);
+        MenuHelper.initStyleMenu(this, jMenuBar);
+        MenuHelper.initAlignMenu(this, jMenuBar);
+        MenuHelper.initColorMenu(this, jMenuBar);
+        MenuHelper.initFontMenu(this, jMenuBar);
+        MenuHelper.initHelpMenu(this, jMenuBar);
+
+        this.getContentPane().add(jMenuBar, BorderLayout.NORTH);
+    }
 
 //    Реализуй метод инициализации панелей редактора initEditor(). Он должен:
 //            6.1. Устанавливать значение "text/html" в качестве типа контента для компонента htmlTextPane.
