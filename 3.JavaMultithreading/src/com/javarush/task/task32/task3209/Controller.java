dd365361dcadd2b74e96443da404b75017dfe7af
Corvit <vcfrolov@gmail.com>
2020-09-30 17:02:36 +0300

    task32.task3209 HTML Editor(15)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.UndoListener;
+
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.Document;
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.undo.UndoManager;
 import java.io.File;
 
 public class Controller {
@@ -44,5 +50,38 @@ public class Controller {
 
     public HTMLDocument getDocument() {
         return document;
+    }
+
+//    HTML Editor (15)
+//    Добавь в контроллер метод resetDocument(), который будет сбрасывать текущий документ. Он должен:
+//            15.1. Удалять у текущего документа document слушателя правок которые можно отменить/вернуть (найди подходящий для этого метод, унаследованный от AbstractDocument).
+//    Слушателя нужно запросить у представления (метод getUndoListener()).
+//    Не забудь проверить, что текущий документ существует (не null).
+//            15.2. Создавать новый документ по умолчанию и присваивать его полю document.
+//
+//    Подсказка: воспользуйся подходящим методом класса HTMLEditorKit.
+//
+//15.3. Добавлять новому документу слушателя правок.
+//15.4. Вызывать у представления метод update().
+//
+//
+//    Требования:
+//            1. Класс Controller должен содержать публичный метод resetDocument(), который будет сбрасывать текущий документ.
+//            2. Метод resetDocument() должен удалять у текущего документа document слушателя правок через метод removeUndoableEditListener().
+//            3. Метод resetDocument() должен создавать новый документ по умолчанию через метод createDefaultDocument().
+//            4. Метод resetDocument() должен добавлять новому документу слушателя правок через метод addUndoableEditListener().
+//            5. Метод resetDocument() должен вызывать у представления метод update().
+    
+
+    public void resetDocument() {
+        UndoListener listener = view.getUndoListener();
+
+        if (document != null) {
+            document.removeUndoableEditListener(listener);
+        }
+
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(listener);
+        view.update();
     }
 }
