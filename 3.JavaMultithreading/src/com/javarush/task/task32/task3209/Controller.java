01849ea9d5fee3415a653866dc1b90f120b0e672
Corvit <vcfrolov@gmail.com>
2020-10-08 11:16:12 +0300

    task32.task3209 HTML Editor(17)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -2,15 +2,15 @@ package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.UndoListener;
 
-import javax.swing.text.AbstractDocument;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
+import javax.swing.text.*;
+import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.undo.UndoManager;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Controller {
     private View view;
@@ -64,5 +64,48 @@ public class Controller {
         {
             ExceptionHandler.log(e);
         }
+    }
+
+    public String getPlainText()
+    {
+        StringWriter stringWriter = new StringWriter();
+        HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+//        ElementIterator iterator = new ElementIterator(this.document);
+//        Element element;
+//        while((element = iterator.next()) != null)
+//        {
+//            AttributeSet attributes = element.getAttributes();
+//            Object name = attributes.getAttribute(StyleConstants.NameAttribute);
+//            if((name instanceof HTML.Tag)
+//                    && ((name == HTML.Tag.H1) || (name == HTML.Tag.H2) || (name == HTML.Tag.H3))) {
+////                StringBuffer text = new StringBuffer();
+//                int count = element.getElementCount();
+//                for (int i = 0; i < count; i++) {
+//                    Element child = element.getElement(i);
+//                    AttributeSet childAttributes = child.getAttributes();
+//                    if(childAttributes.getAttribute(StyleConstants.NameAttribute) == HTML.Tag.CONTENT) {
+//                        int startOffset = child.getStartOffset();
+//                        int endOffset = child.getEndOffset();
+//                        int length = endOffset - startOffset;
+//                        try{
+//                            htmlEditorKit.write(stringWriter);
+////                            text.append(document.getText(startOffset,length));
+//                        } catch (BadLocationException e)
+//                        {
+//                            ExceptionHandler.log(e);
+//                        }
+//                    }
+//                }
+//            }
+//        }
+        try {
+            //this.document.getRootElements();
+            htmlEditorKit.write(stringWriter, this.document, 0, document.getLength());
+            //htmlEditorKit.write(stringWriter, this.document, 0, text.length());
+        } catch (IOException | BadLocationException e)
+        {
+            ExceptionHandler.log(e);
+        }
+        return stringWriter.toString();
     }
 }
