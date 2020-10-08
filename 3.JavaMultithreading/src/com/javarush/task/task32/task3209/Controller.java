9268eeb0ab025f17e60d291c3a6da10f982c9376
Corvit <vcfrolov@gmail.com>
2020-10-08 11:50:19 +0300

    task32.task3209 HTML Editor(19)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -32,8 +32,6 @@ public class Controller {
         view.setController(controller);
         view.init();
         controller.init();
-        view.showAbout();
-
     }
 
     public void init(){}
@@ -70,42 +68,18 @@ public class Controller {
     {
         StringWriter stringWriter = new StringWriter();
         HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
-//        ElementIterator iterator = new ElementIterator(this.document);
-//        Element element;
-//        while((element = iterator.next()) != null)
-//        {
-//            AttributeSet attributes = element.getAttributes();
-//            Object name = attributes.getAttribute(StyleConstants.NameAttribute);
-//            if((name instanceof HTML.Tag)
-//                    && ((name == HTML.Tag.H1) || (name == HTML.Tag.H2) || (name == HTML.Tag.H3))) {
-////                StringBuffer text = new StringBuffer();
-//                int count = element.getElementCount();
-//                for (int i = 0; i < count; i++) {
-//                    Element child = element.getElement(i);
-//                    AttributeSet childAttributes = child.getAttributes();
-//                    if(childAttributes.getAttribute(StyleConstants.NameAttribute) == HTML.Tag.CONTENT) {
-//                        int startOffset = child.getStartOffset();
-//                        int endOffset = child.getEndOffset();
-//                        int length = endOffset - startOffset;
-//                        try{
-//                            htmlEditorKit.write(stringWriter);
-////                            text.append(document.getText(startOffset,length));
-//                        } catch (BadLocationException e)
-//                        {
-//                            ExceptionHandler.log(e);
-//                        }
-//                    }
-//                }
-//            }
-//        }
         try {
-            //this.document.getRootElements();
             htmlEditorKit.write(stringWriter, this.document, 0, document.getLength());
-            //htmlEditorKit.write(stringWriter, this.document, 0, text.length());
         } catch (IOException | BadLocationException e)
         {
             ExceptionHandler.log(e);
         }
         return stringWriter.toString();
     }
+
+    public void createNewDocument() {}
+    public void openDocument() {}
+    public void saveDocument() {}
+    public void saveDocumentAs() {}
+
 }
