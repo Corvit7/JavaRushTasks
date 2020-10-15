4bf2248e410e07019253720928949e8a34aa7838
Corvit <vcfrolov@gmail.com>
2020-10-15 09:56:09 +0300

    ShutdownNow!; task2812; Уровень 8, лекция 8

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2812/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task28.task2812;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/* 
+ShutdownNow!
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 1; i <= 10; i++) {
+            final int localId = i;
+            executor.submit(new Runnable() {
+                public void run() {
+                    doExpensiveOperation(localId);
+                }
+            });
+        }
+
+//        executor.shutdownNow().forEach(System.out::println);
+        List<Runnable> list = executor.shutdownNow();
+        for (Runnable item: list) {
+            System.out.println(item.toString() + "was not completed");
+        }
+    }
+
+    private static void doExpensiveOperation(int localId) {
+        System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
+    }
+}
