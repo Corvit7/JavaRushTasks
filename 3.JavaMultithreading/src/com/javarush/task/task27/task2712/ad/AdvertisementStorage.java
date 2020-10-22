3ec883b59e71d16449d6f459462004e92bcf794e
Corvit <vcfrolov@gmail.com>
2020-10-22 06:27:51 +0300

    Ресторан(7)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,9 +1,26 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class AdvertisementStorage {
     private static AdvertisementStorage instance;
+    private final List<Advertisement> videos = new LinkedList<>();
 
-    private AdvertisementStorage(){}
+    public List<Advertisement> list(){
+        return videos;
+    }
+
+    public void add(Advertisement ad) {
+        videos.add(ad);
+    }
+
+    private AdvertisementStorage(){
+        Object someContent = new Object();
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60)); //10 min
+    }
 
     public static AdvertisementStorage getInstance() {
         if (instance == null) {
