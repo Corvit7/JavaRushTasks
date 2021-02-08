98d652fd2a5bf4caf46d97f1820ca58ab461a465
Corvit <vcfrolov@gmail.com>
2021-02-08 14:29:36 +0300

    Построй дерево(2)

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -4,6 +4,8 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.Collection;
+import java.util.List;
 
 /* 
 Построй дерево(1)
@@ -14,13 +16,52 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
     @Override
     public String get(int index) {
-        return null;
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String set(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public int size() {
         return 0;
     }
+
+
+//    public String get(int index)
+//    public String set(int index, String element)
+//    public void add(int index, String element)
+//    public String remove(int index)
+//    public List<String> subList(int fromIndex, int toIndex)
+//    protected void removeRange(int fromIndex, int toIndex)
+//    public boolean addAll(int index, Collection<? extends String> c)
 
 
 }
