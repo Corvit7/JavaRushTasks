8d21a4bd0dbf7c966769752b6a96c60bf7e8b0e8
Corvit <vcfrolov@gmail.com>
2021-03-30 16:46:52 +0300

    Space(7)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -1,31 +1,22 @@
 package com.javarush.task.task25.task2515;
 
-//В классе BaseObject необходимо:
-//        а) Добавить переменные x (double), y (double), radius (double), геттеры и сеттеры для них.
-//        б) Добавить логическую переменную isAlive (жив объект или уже нет).
-//        в) Добавить геттер (isAlive()-метод для isAlive-переменной).
-//        г) Добавить конструктор BaseObject(double x, double y, double radius).
-//        д) Проследить, чтобы в конструкторе isAlive устанавливался в true (мертворожденные нам ни к чему).
-//        Также надо пройтись по всем классам-наследникам и поправить у них конструкторы.
-//        Если ты используешь Intellij IDEA - Alt+Insert тебе в помощь.
-//
-
-
+/**
+ * Базовый класс для всех объектов игры.
+ */
 public abstract class BaseObject {
-    private double x;
-    private double y;
-    private double radius;
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
+    //состояние объект - жив ли объект
     private boolean isAlive;
 
     public BaseObject(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        isAlive = true;
-    }
-
-    public boolean isAlive() {
-        return isAlive;
+        this.isAlive = true;
     }
 
     public double getX() {
@@ -52,16 +43,50 @@ public abstract class BaseObject {
         this.radius = radius;
     }
 
-    public void draw() {}
-    public void move() {}
-    public void die() {isAlive = false;}
-
-    public boolean isIntersect(BaseObject o)
-    {
-        if (Math.sqrt(Math.pow(this.x - o.getX(), 2) + Math.pow(this.y - o.getY(), 2)) < Math.max(this.radius, o.radius))
-            return true;
-        else
-            return false;
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    public void draw(Canvas canvas) {
+        //do nothing
     }
 
+    /**
+     * Двигаем себя на один ход.
+     */
+    public void move() {
+        //do nothing
+    }
+
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    public void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public void die() {
+        isAlive = false;
+    }
+
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
+    public boolean isIntersect(BaseObject o) {
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double distance = Math.sqrt(dx * dx + dy * dy);
+        double distance2 = Math.max(radius, o.radius);
+        return distance <= distance2;
+    }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -1,4 +1,32 @@
 package com.javarush.task.task25.task2515;
 
+//Требования:
+//        1. В классе Canvas создай приватное int поле width. Добавь для него getter.
+//        2. В классе Canvas создай приватное int поле height. Добавь для него getter.
+//        3. В классе Canvas создай приватное поле matrix (char[][]). Добавь для него getter.
+//        4. В классе Canvas создай конструктор Canvas(int width, int height). Инициализируй поля width и height.
+//        5. Инициализируй в конструкторе поле matrix (char[height][width]).
+
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
 }
