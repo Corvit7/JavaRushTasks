07274c19871bfd6f2b52337a01825386452235d5
Виктор Фролов <vcfrolov@gmail.com>
2021-04-11 11:22:30 +0300

    AmigoSet(2)

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,11 +1,32 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class AmigoSet<E> extends AbstractSet<E> implements Cloneable, Serializable, Set<E> {
+
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E,Object> map;
+
+    public AmigoSet() {
+        this.map = new HashMap<>();
+    }
+
+    @Override
+    public boolean add(E e) {
+        return null == map.put(e, PRESENT);
+    }
+
+//    public HashMap<E, Object> getMap() {
+//        return map;
+//    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        int capacity = Math.max(16, (int)Math.ceil(collection.size()/.75f));
+        this.map = new HashMap<>(capacity);
+        this.addAll(collection);
+
+    }
 
     @Override
     public Iterator<E> iterator() {
@@ -16,4 +37,15 @@ public class AmigoSet<E> extends AbstractSet<E> implements Cloneable, Serializab
     public int size() {
         return 0;
     }
+
+    public static void main(String[] args) {
+        Collection<Integer> src = new HashSet<>();
+        src.add(1);
+        src.add(2);
+        src.add(3);
+        AmigoSet<Integer> dst = new AmigoSet<>(src);
+//        HashMap test = dst.getMap();
+//        AmigoSet test = new
+    }
+
 }
