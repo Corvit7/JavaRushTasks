f976ba049a39356663f2781e1d61922e4a50acdd
Corvit <vcfrolov@gmail.com>
2021-04-15 15:27:04 +0300

    task15.task1522 Закрепляем паттерн Singleton

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1522/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task15.task1522;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Закрепляем паттерн Singleton
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(thePlanet);
+    }
+
+    public static Planet thePlanet;
+
+    //add static block here - добавьте статический блок тут
+    static {
+        readKeyFromConsoleAndInitPlanet();
+    }
+
+    public static void readKeyFromConsoleAndInitPlanet() {
+        // implement step #5 here - реализуйте задание №5 тут
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String inp = "";
+        try {
+            inp = bufferedReader.readLine();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        switch (inp)
+        {
+            case Planet.EARTH:
+                thePlanet = Earth.getInstance();
+                break;
+            case Planet.MOON:
+                thePlanet = Moon.getInstance();
+                break;
+            case Planet.SUN:
+                thePlanet = Sun.getInstance();
+                break;
+            default:
+                thePlanet = null;
+                break;
+        }
+    }
+}
