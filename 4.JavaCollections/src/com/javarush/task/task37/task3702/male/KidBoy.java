632778b3aeb8f6aeb6c61d5b5fe5e62868dfe546
Corvit <vcfrolov@gmail.com>
2021-04-20 12:47:49 +0300

    Factory(2)

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/male/KidBoy.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/KidBoy.java
@@ -3,7 +3,7 @@ package com.javarush.task.task37.task3702.male;
 import com.javarush.task.task37.task3702.Human;
 
 public class KidBoy implements Human {
-    public static final int MAX_AGE = 12;
+    public static final int MAX_AGE = 13;
 
     @Override
     public String toString() {
---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+public class MaleFactory implements Human {
+    public Human getPerson(int age) {
+        if( age >= 0 && age < KidBoy.MAX_AGE)
+            return new KidBoy();
+        else if( age > 12 && age < 20)
+            return new TeenBoy();
+        else
+            return new Man();
+    }
+}
