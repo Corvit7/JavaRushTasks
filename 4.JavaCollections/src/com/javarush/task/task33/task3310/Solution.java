77ecb4daada4444887976ab371adad2ca59d6b72
Виктор Фролов <vcfrolov@gmail.com>
2021-05-05 12:08:39 +0300

    Shortener(4)

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -1,7 +1,11 @@
 package com.javarush.task.task33.task3310;
 
+import java.io.IOException;
+
 public class Solution {
     public static void main(String[] args) {
+        System.out.println(new Helper().generateRandomString());
+        ExceptionHandler.log(new IOException("wrong"));
 
     }
 }
