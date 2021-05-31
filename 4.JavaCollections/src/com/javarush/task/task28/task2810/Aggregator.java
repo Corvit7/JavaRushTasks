275f2cc0f7bb89392f695dcad4d39b60641a77a5
Corvit <vcfrolov@gmail.com>
2021-05-31 14:07:23 +0300

    Aggregator(4)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.HHStrategy;
 import com.javarush.task.task28.task2810.model.Provider;
 
 public class Aggregator {
