71b36dd354149cf670e218b52459364718c91425
Виктор Фролов <vcfrolov@gmail.com>
2021-06-07 17:29:32 +0300

    Aggergator(6)

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task28.task2810.model;
+
+import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HHStrategy implements Strategy{
+    private static final String URL_FORMAT = "https://hh.ru/search/vacancy?text=java+%s&page=%d";
+
+    @Override
+    public List<Vacancy> getVacancies(String searchString)  {
+        Document doc;
+        try {
+            Document document = Jsoup.connect(String.format(URL_FORMAT, "MyTown", 0)).get();
+            System.out.println("");
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+}
